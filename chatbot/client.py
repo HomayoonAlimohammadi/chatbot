9867e0b07d80295f65d0eac4398c70c559244fb7
homayoonalimohammadi <homayoonalimohammadi@gmail.com>
2022-07-27 08:30:57 +0430

    Refactored, Fixed multithreaded client

--- chatbot/client.py
+++ chatbot/client.py
@@ -1,16 +1,38 @@
+from __future__ import annotations
+from concurrent.futures import Future
+from concurrent.futures import ThreadPoolExecutor
 import socket
 import json
+from time import sleep
+from typing import Any
 
 
-with open("conf.json", "r") as f:
-    data = json.load(f)
-    HOST = data["HOST"]
-    PORT = data["PORT"]
-    print(HOST, PORT)
-
-
-for i in range(5):
+def send_request(HOST: str, PORT: int) -> None:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.sendall(b"my request")
-        data = s.recv(1024).decode()
+
+
+def check_done(works: list[Future[Any]]):
+    return all([work.done() for work in works])
+
+
+def main():
+    with open("conf.json", "r") as f:
+        data = json.load(f)
+        HOST = data["HOST"]
+        PORT = data["PORT"]
+        NUM_THREADS = data["NUM_THREADS"]  # 5
+        print(HOST, PORT)
+    with ThreadPoolExecutor(NUM_THREADS) as executor:
+        works = [executor.submit(send_request, HOST, PORT) for _ in range(NUM_THREADS)]
+
+    sleep(1)
+    if not check_done(works):
+        print("Something went wrong...")
+    else:
+        print("Every client got their answer.")
+
+
+if __name__ == "__main__":
+    main()
--- chatbot/server.py
+++ chatbot/server.py
@@ -1,47 +1,77 @@
 from __future__ import annotations
 from concurrent.futures import ThreadPoolExecutor
-from time import sleep, time
+from time import sleep
 import socket
-from queue import Queue
 from typing import Any
 import json
 
 
+class NoSocketAvailableException(Exception):
+    ...
+
+
 class ChatBot:
+    """
+    ChatBot class for accepting requests and establishing connections with the client.
+    """
+
     def __init__(self, host: str, port: int, n_threads: int = 5) -> None:
         self._host = host
         self._port = port
         self._n_threads = n_threads
-        self.works: list[Any] = []
+        self.works: list[Any] = []  # kheili bade!
 
     def run(self) -> None:
-        print("ChatBot is starting quickly...")
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self._host, self._port))
-        self.listen()
+        """
+        Function that gets the chatbot instance running.
+        """
+        try:
+            print("ChatBot is starting quickly...")
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind((self._host, self._port))
+            self.accept_connection()
+        except Exception as e:
+            print(e)
+            # TODO: user some logging tool or...
+            raise NoSocketAvailableException
 
-    def listen(self) -> None:
+    def accept_connection(self) -> None:
+        """
+        Function that accepts new connections and pass it to the handler.
+        """
         self.socket.listen()
         with ThreadPoolExecutor(self._n_threads) as executor:
             while True:
                 try:
                     conn, addr = self.socket.accept()
+                    print(f"New Connection with address {addr} was added.")
                     work = executor.submit(self.handler, conn)
                     self.works.append(work)
                 except KeyboardInterrupt as e:
-                    self.stop()
+                    print(e)
+                    self.shutdown()
+                    break
 
     def handler(self, conn: socket.socket) -> None:
+        """
+        Handles a given connection.
+        Closes the connection at the end.
+        """
         sleep(1)
         with conn:
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
+                # TODO: Check for the connection not being closed.
                 conn.sendall("Done".encode())
 
-    def stop(self):
+    def shutdown(self) -> None:
+        """
+        Shutdown the chatbot after ensuring all the connections are dealt with.
+        """
         print("ChatBot is shuttoing down gracefully...")
+        # TODO: maybe the client closes the connection and...
         self.socket.close()
         for work in self.works:
             if not work.done():
@@ -51,15 +81,13 @@ class ChatBot:
             print("All Done.")
 
 
-def main():
+def main() -> None:
     with open("conf.json", "r") as f:
         data = json.load(f)
         HOST = data["HOST"]
         PORT = data["PORT"]
     chatbot = ChatBot(HOST, PORT)
     chatbot.run()
-    sleep(2)
-    chatbot.stop()
 
 
 if __name__ == "__main__":
